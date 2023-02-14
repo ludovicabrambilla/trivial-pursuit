218d0cbf84157d5ceced7b31ed08158b8fb2b108
ludovicabrambilla <ludovicamariabrambilla@gmail.com>
2023-02-14 12:11:52 +0100

    Complete question list (20/category) and options

--- questions.py
+++ questions.py
@@ -30,10 +30,10 @@ questions = {
             "Web browsers", "Search engines", "Mail services", "Cloud providers"
         ],
         "What was Twitter’s original name?": [
-            "twttr", "Twitter", "bird"
+            "twttr", "Twitter", "bird", "Facebook"
         ],
         "Who discovered penicillin?": [
-            "Alexander Fleming", "Marie Curie", "Nobody", "Antoine Lavoisier"
+            "Alexander Fleming", "Marie Curie", "Aristotle", "Antoine Lavoisier"
         ],
         "Who was the first woman to win a Nobel Prize (in 1903)?": [
             "Marie Curie", "Ada Lovelace", "Rita Levi-Montalcini", "Elizabeth Blackwell"
@@ -116,51 +116,261 @@ questions = {
             "Hugh Jackman", "Jake Gyllenhaal", "Robert Downey Jr", "Ryan Reynolds"
         ],
         "Which actress won the Oscar for Best Actress in 2003?": [
-            "Nicole Kidman"
+            "Nicole Kidman", "Angelina Jolie", "Viola Davis", "Meryl Streep"
         ],
         "The Young Victoria was played by which British actress?": [
-            "Emily Blunt"
+            "Emily Blunt", "Helen Mirren", "Kate Winslet", "Maggie Smith"
         ],
         "Who did Forbes name the youngest self-made billionaire in 2019?": [
-            "Kylie Jenner"
+            "Kylie Jenner", "Jack Cowin", "Kim Beom-Su", "Jeff Bezos"
         ]
     },
     categories['brown']: {
-        "Who played Anita in the first Broadway production of West Side Story?": [
-            "Chita Rivera"
-        ],
         "In which US city is Broadway located?": [
-            "New York City"
+            "New York City", "Los Angeles", "Las Vegas", "Washington D.C."
         ],
         "What is the longest-running Broadway show?": [
-            "The Phantom of the Opera"
+            "The Phantom of the Opera", "The Lion King", "Chicago", "Cats"
         ],
-        "Which show is the highest-grossing production on Broadway ever?": [
-            "The Lion King"
+        "During which time of the year is the Nutcracker primarily performed?": [
+          "During Christmas season", "During Spring season", "During School season", "During Easter season"  
         ],
-        "In what year was Broadway established?": [
-            "1750"
+        "What was the name of the rock band formed by Jimmy Page?": [
+            "Led Zeppelin", "Queen", "The Beatles", "Metallica"
         ],
-        "In which period did ballet originate?": [
-            "Italian Renaissance"
+        "What genre of music did Taylor Swift start in?": [
+           "Country", "Pop", "Soft Rock", "Classical" 
         ],
-        "During which time of the year is the Nutchracker primarily performed?": [
-          "During Christmas season"  
+        "Which musical legend is Jay-Z married to?": [
+            "Beyonce", "Rihanna", "Britney Spears", "Whitney Houston"
         ],
-        "What French ballet term means \'in front\'?": [
-            "Devant"
+        "How many Grammys does John Legend have?": [
+            "Ten", "Two", "Zero", "Five"
         ],
-        "What is a plié in ballet?": [
-            "Bending at the knees"
+        "Which British girl group had a member by the name of Mel B?": [
+            "Spice Girls", "All Saints", "Electrovamp", "The Harmonies"
+        ],
+        "Who was the messenger of the gods?": [
+            "Hermes", "Mars", "Zeus", "Uranus"
+        ],
+        "In Norse mythology, what’s the name of the paradise warriors go to after death?": [
+            "Valhalla", "Paradise", "Ade", "Underworld"
+        ],
+        "Thor was the son of which God?": [
+            "Odin", "Zeus", "Baldr", "Loki"
+        ],
+        "Romulus and Remus were raised by what animal?": [
+            "A she-wolf", "A horse", "A pig", "A monkey"
+        ],
+        "What was the name of the Egyptian God of the Sun?": [
+            "Ra", "Isis", "Set", "Toth"
+        ],
+        "Who wrote “Old Man and The Sea” and is considered one of the greatest writers of the 20thcentury?": [
+            "Ernest Hemingway", "J.R.R. Tolkien", "J.D. Salinger", "George Orwell"
+        ],
+        "What Danish author is considered by many to be the most prolific fairy tale writer?": [
+            "Hans Christian Andersen", "Karen Blixen", "Ejnar Mikkelsen", "Ole Sarvig"
+        ],
+        "Who is the writer of “Merchant of Venice”?": [
+            "William Shakespeare", "Carlo Goldoni", "Ernest Hemingway", "Marco Polo"
+        ],
+        "'Adventure of Sherlock Holmes' was written by which writer?": [
+            "Arthur Conan Doyle", "John LeCarré", "Agatha Chirstie", "Stephen King"
+        ],
+        "What is the name of the fourth book in the Harry Potter series?": [
+            "Harry Potter and the Goblet of Fire", "Harry Potter and the Sorcerer’s Stone", "Harry Potter and the Order of the Phoenix", "Harry Potter and the Deathly Hallows"
+        ],
+        "Which country is Aesop’s Fables believed to originate in?": [
+            "Greece", "Italy", "Egypt", "China"
+        ],
+        "The book 'Da Vinci Code', was written by whom?": [
+            "Dan Brown", "Khaled Hosseini", "John Grishman", "Jane Austen"
         ]
     },
     categories['yellow']: {
-
+        "How many Pyramids of Giza were made?": [
+            "Three", "One", "Two", "Five"
+        ],
+        "Where did the Franks settle after defeating the Romans?": [
+            "Gaul", "Normandy", "Occitania", "Provence-Alpes-Cote d'Azur"
+        ],
+        "Which religion dominated the Middle Ages?": [
+            "Catholicism", "Buddhism", "Confucianism", "Protestantism"
+        ],
+        "In which year World War I begin?": [
+            "1914", "910", "1915", "1814"
+        ],
+        "In which country Adolph Hitler was born?": [
+            "Austria", "Germany", "Poland", "Belgium"
+        ],
+        "John F. Kennedy was assassinated in which city?": [
+            "Dallas", "Austin", "Houston", "Laredo" 
+        ],
+        "Which American president was involved in the Watergate scandal?": [
+            "Nixon", "Adams", "Roosevelt", "Reagan"
+        ],
+        "Who has been the longest-serving monarch of the United Kingdom?": [
+           "Queen Elizabeth II", "King George I", "Queen Victoria", "King George VI"
+        ],
+        "World War I began with the death of Archduke Franz Ferdinand, of which country?": [
+            "Austria", "Germany", "Poland", "Belgium"
+        ],
+        "Leader and political icon Nelson Mandela was from which African country?": [
+            "South Africa", "Kenia", "Congo", "Senegal"
+        ],
+        "Who sent Christopher Columbus to explore the New World?": [
+            "King Ferdinand of Spain", "It was his own initiative", "Queen Victoria of England", "François 1er of France" 
+        ],
+        "What is considered the largest empire in history?": [
+            "The Mongol Empire", "The Roman Empire", "The British Empire", "The Ottoman Empire"
+        ],
+        "What year did the French Revolution start?": [
+            "1789", "1912", "1495", "1815"
+        ],
+        "Where did Albert Einstein live before moving to the United States?": [
+            "Germany", "Belgium", "Spain", "Netherlands"
+        ],
+        "Who is commonly referred to as the person who created the first printing press?": [
+            "Johannes Gutenberg", "Martin Luther", "Thomas Edison", "Nikola Tesla"
+        ],
+        "What was the code name for the German invasion of the Soviet Union during World War II?": [
+            "Operation Barbarossa", "Operation Overlord", "Operation Red Dawn", "Operation Valkyrie"
+        ],
+        "Who was the first ruler of the Mongol Empire?": [
+            "Genghis Khan", "Tolui Khan", "Oghul Qaimish", "Kublai Khan"
+        ],
+        "What year did India gain independence from Britain?": [
+            "1947", "1945", "1876", "1990"
+        ],
+        "Who was the first person in the world to land on the moon and which country were they from?": [
+            "Neil Armstrong from the United States", "Yuri Gagarin from Soviet Union", "Valentina Tereshkova from Soviet Union", "Alan Shepard from United States"
+        ],
+        "What was the name of the Ukrainian nuclear power plant that was the site of a nuclear disaster in April 1986?": [
+            "Chernobyl", "Fukushima", "KORI", "Qinshan"
+        ]
     },
-    categories['green']: {
-
+    categories['blue']: {
+        "What is the name of the thin and long country that spans more than half of the western coast of South America?": [
+            "Chile", "Peru", "Argentina", "Brasil"
+        ],
+        "Which American state is the largest (by area)?": [
+           "Alaska", "Texas", "Idaho", "California"
+        ],
+        "Which two countries share the longest international border?": [
+            "Canada and the USA", "USA and Mexico", "France and Italy", "Russia and China"
+        ],
+        "What is the smallest country in the world?": [
+            "Vatican City", "Grand Duchy of Luxembourg", "Principality of Monaco"
+        ],
+        "Which continent is the largest?": [
+            "Asia", "Africa", "Oceania", "America"
+        ],
+        "Which of the Seven Wonders is located in Egypt?": [
+            "The Pyramids of Giza", "The Colosseum", "Taj Mahal", "Petra"
+        ],
+        "What is the capital of New Zealand?": [
+            "Wellington", "Perth", "Queenstown", "Rome"
+        ],
+        "Which hot desert is the largest in the world?": [
+            "The Sahara Desert", "Kalahari", "Mojave", "Great Victoria"
+        ],
+        "What is the name of the world’s longest river?": [
+            "The Nile", "The Amazon", "The Yangtze", "The Mississippi"
+        ],
+        "Which city in India would you find the Taj Mahal in?": [
+            "Agra", "Mumbai", "Bangalore", "Delhi"
+        ],
+        "What present-day Italian city does Mt. Vesuvius overlook?": [
+            "Naples", "Florence", "Rome", "Milan"
+        ],
+        "What is the capital of Thailand?": [
+            "Bangkok", "Chiang Mai", "Ko Samui", "Phuket"
+        ],
+        "What country has the most natural lakes?": [
+            "Canada", "US", "Italy", "Switzerland"
+        ],
+        "What is the name of the river that flows through the Brazil rainforest?": [
+            "The Amazon", "Rio Grande", "Mississippi", "Ob"
+        ],
+        "What is the capital of Canada?": [
+            "Ottawa", "Toronto", "Vancouver", "Alberta"
+        ],
+        "Where is the Eiffel Tower located?": [
+            "Paris, France", "Rome, Italy", "New York, US", "Marseiile, France"
+        ],
+        "What American city is the Golden Gate Bridge located in?": [
+            "San Francisco", "san Diego", "Los Angeles", "New York"
+        ],
+        "Which country has the largest population in the world?": [
+            "China", "Russia", "South Africa", "India"
+        ],
+        "What is the name of the tallest mountain in the world?": [
+            "Mount Everest", "K2", "Kangchenjunga", "Makalu"
+        ],
+        "What planet is closest to Earth?": [
+            "Venus", "Mars", "Jupiter", "Saturn"
+        ]
     },
     categories['orange']: {
-        
+        "Who is often credited with creating the world’s first car?": [
+            "Karl Benz", "Henry Ford", "Frederick Taylor", "Wilbur Wright"
+        ],
+        "Which animal can be seen on the Porsche logo?": [
+           "Horse", "Jaguar", "Eagle", "Bear"
+        ],
+        "Which companies are part of the Big Three?": [
+            "General Motors, Fiat Chrysler Automobiles, and Ford Motor Company", "Volkswagen Group, Daimler AG, BMW", "Toyota Motor Corporation, Honda, Nissan", "Tesla Inc., Ford, Peugeot "
+        ],
+        "Which company owns Bugatti, Lamborghini, Audi, Porsche, and Ducati?": [
+            "Volkswagen Group", "BMW", "Audi", "Tesla Inc."
+        ],
+        "What does BMW stand for (in English)?": [
+            "Bavarian Motor Works", "Brave Motor Works", "BMW", "Best Motor Work"
+        ],
+        "What or who is the Ford Mustang named after?": [
+            "A fighter plane from WWII", "A horse", "A friend of Henry Ford", "It is invented"
+        ],
+        "How many parts (screws and bolts included) does the average car have?": [
+           "30.000", "10.000", "5.000", "300"
+        ],
+        "Which car is often called the first muscle car?": [
+            "The Pontiac GTO", "Pontiac Firebird", "Chevrolet Camaro", "Ford Mustang Boss 429"
+        ],
+        "In what year was the Corvette introduced?": [
+            "1953", "1946", "1975", "1990"
+        ],
+        "How many soccer players should each team have on the field at the start of each match?": [
+            "11", "22", "13", "26"
+        ],
+        "When Michael Jordan played for the Chicago Bulls, how many NBA Championships did he win?": [
+            "Six", "Five", "One", "Three"
+        ],
+        "Which Williams sister has won more Grand Slam titles?": [
+            "Serena", "Venus"
+        ],
+        "What country won the very first FIFA World Cup in 1930?": [
+            "Uruguay", "Paraguay", "Brasil", "Argentina"
+        ],
+        "In what year was the first-ever Wimbledon Championship held?": [
+            "1877", "1900", "1899", "1866"
+        ],
+        "Which racer holds the record for the most Grand Prix wins?": [
+            "Lewis Hamilton", "Michael Schumacher", "Sebastian Vettel", "Alain Prost"
+        ],
+        "Which Jamaican runner is an 11-time world champion and holds the record in the 100 and 200-meter race?": [
+            "Usain Bolt", "Elaine Thompson", "Shelly-Ann Fraser-Pryce", "Asafa Powell"
+        ],
+        "Which boxer was known as “The Greatest” and “The People’s Champion”?": [
+            "Muhammad Ali", "Sugar Ray Robinson", "Roberto Duran", "Henry Armstrong"
+        ],
+        "What sport was Jesse Owens involved in?": [
+            "Track and field", "Swimming", "Boxing", "Football"
+        ],
+        "Which hockey team did Wayne Gretzky play for in the '80s?": [
+           "Edmonton Oilers", "Calgary Flames", "Colorado Rockies", "New York Islanders"
+        ],
+        "What were the first two women’s sports included in the modern Olympics?": [
+            "Tennis and golf (1900)", "Fencing and Athletics (1896)", "Archery and rowing (1904)", "Artistic Gymnastics and athletics (1908)"
+        ]
     }
 }
