749c9355dbb45361dd27e3c7d64a5982bcccfa3d
ludovicabrambilla <ludovicamariabrambilla@gmail.com>
2023-02-15 11:39:47 +0100

    Refactor init, ask question and roll die

--- trivial.py
+++ trivial.py
@@ -13,32 +13,36 @@ class Player:
 
     # Function to check if the input provided from the user is valid
     # Takes the input prompt and value, the list where you want to check if the value exists, the error message to display if the input is not valid
-    def validate_input(self, user_input, validation_list, error_message):
+    def validate_input(self, validation_list, user_input, error_message):
         while True:
             label = input(user_input).strip().lower()
             if label in validation_list:
                 return label
             print(error_message)
-
     
+    # Function to print available options in format "a) option"
+    def print_options(self, options_list):
+        labeled_options = dict(zip(ascii_lowercase, options_list))
+        for label, option in labeled_options.items():
+            print(f" {label}) {option}")
+        return labeled_options
 
-    # Initialize the player. Ask for name and token they want to choose.
-    def __init__(self):
-        # USER CHOOSES A NAME FOR THE GAME
+    # Player chooses name
+    def choose_name(self):
         name = input('Choose a name for your player: ').strip()
-        self.name = name
+        return name
 
-        # USER CHOOSES A TOKEN
+    # Player chooses token
+    def choose_token(self):
         # Print the available tokens
         print('These are the available tokens:')
-        labeled_available_tokens = dict(zip(ascii_lowercase, Player.available_tokens))
-        for label, token in labeled_available_tokens.items():
-            print(f'{label}) {token}')
-        # Check if the input is valid
-        token_label_prompt = 'Choose one token. Type the corresponding letter to make your choice: '
-        token_error_message = f"Please choose one of the available tokens: {', '.join(labeled_available_tokens)}"
-        token_checked = self.validate_input(token_label_prompt, labeled_available_tokens, token_error_message)
-
+        labeled_available_tokens = self.print_options(Player.available_tokens)
+        # Validate the choice
+        token_checked = self.validate_input(
+            labeled_available_tokens,
+            'Choose one token. Type the corresponding letter to make your choice: ',
+            f"Please choose one of the available tokens: {', '.join(labeled_available_tokens)}"
+        )
         # Store the valid chosen token value in a variable
         token = labeled_available_tokens[token_checked]
         # Remove it from the available tokens
@@ -46,12 +50,13 @@ class Player:
         # Add it to the taken_tokens
         Player.taken_tokens[self.name] = token
         print(f"Your token is {token}.")
-        # TODO: debug functions
-        print(Player.available_tokens)
-        print(Player.taken_tokens)
-        
-        # Store the token as an instance variable
-        self.token = Player.taken_tokens[self.name]
+        return Player.taken_tokens[self.name]
+
+
+    # Initialize the player. Ask for name and token they want to choose.
+    def __init__(self):
+        self.name = self.choose_name()        
+        self.token = self.choose_token()
 
 
     # Prompt a question to the player
@@ -65,16 +70,15 @@ class Player:
         options = questions[category][question]
         # the correct answer is always the first in the options list
         correct_answer = options[0]
-        # shuffle the order of the options with a random sample that takes all the available options
-        # present the options as "a) option"
-        labeled_options = dict(zip(ascii_lowercase, random.sample(options, k=len(options))))
-        for label, option in labeled_options.items():
-            print(f" {label}) {option}")
+        # shuffle the order of the options with a random sample that takes all the available options and print them
+        labeled_options = self.print_options(random.sample(options, k=len(options)))
 
         # ERROR HANDLING: Reprompt the player if he/she gives an answer not in the options given
-        answer_label_prompt = f"\nChoice? "
-        answer_error_message = f"Please answer one of {', '.join(labeled_options)}"
-        answer_checked = self.validate_input(answer_label_prompt, labeled_options, answer_error_message)
+        answer_checked = self.validate_input(
+            labeled_options,
+            f"\nChoice? ",
+            f"Please answer one of {', '.join(labeled_options)}"
+            )
         answer = labeled_options[answer_checked]
         if answer == correct_answer:
             num_correct += 1
@@ -96,15 +100,13 @@ class Player:
             self.roll_die()
         # if die goes on Start, the user must choose a category
         elif category == categories['start']:
-            labeled_categories = dict(zip(ascii_lowercase, list(questions)))
-            print('Choose a category for your question:')
-            for label, question_category in labeled_categories:
-                print(f" {label}) {question_category}")
-
+            labeled_categories = self.print_options(list(questions))
             # check if the provided category is valid
-            category_label_prompt = f"\nChoice? "
-            category_error_message = f"Please choose one of the categories {', '.join(labeled_categories)}"
-            category_checked = self.validate_input(category_label_prompt, labeled_categories, category_error_message)
+            category_checked = self.validate_input(
+                labeled_categories,
+                f"\nChoice? ",
+                f"Please choose one of the categories {', '.join(labeled_categories)}"
+                )
             # the category is chosen from the user
             category = labeled_categories[category_checked]
 
