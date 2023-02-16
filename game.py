6101bbcbfe3029d5f9d0c087baf97dcb786aec47
ludovicabrambilla <ludovicamariabrambilla@gmail.com>
2023-02-16 11:44:34 +0100

    Automated the creation of player and tokens. Improve the method assignment among the classes

--- game.py
+++ game.py
@@ -1,14 +1,66 @@
+import random
 from trivial import Player
+from utility import validate_option, print_options
+from questions import default_questions
+from board import squares, special_squares
+
+
+
 
 class Game:
+    colors = ['blue', 'brown', 'green', 'yellow', 'orange', 'pink']
+    board = squares
+    special = special_squares
+
+    tokens = [i for i in colors]
+
     min_players = 2
     max_players = 6
 
     def __init__(self):
+        # Game setup
+        self.customize = self.ask_if_customize()
         self.players_num = self.get_number_of_players()
         self.players = []
-        self.create_players(self.players_num)
+        self.available_tokens = Game.tokens[:self.players_num]
+        self.taken_tokens = {}
 
+        self.create_players(self.players_num)
+    
+
+    # Ask the user if they want to customize their categories and questions
+    def ask_if_customize(self):
+        print("The game provides standard categories and questions for each category.\nHowever, you can make your own categories and questions.")
+        while True:
+            customize = input("Do you want to create your own categories (y/n)? ").strip().lower()
+            if customize == 'y':
+                self.customize = True
+                # call the function to create the categories
+                self.create_categories()
+                # call the function to create the questions
+                self.create_questions()
+                return True
+            elif customize == 'n':
+                # provide the default categories
+                category_names = ['Geography', 'Arts & Literature', 'Science & Nature', 'History','Sports & Leisure', 'Entertainment']
+                self.categories = dict(zip(Game.colors, category_names))
+                # provide the default questions
+                self.questions = default_questions
+                return False
+            
+            print('Please enter a valid answer ("y" or "n").')
+
+
+    # Allow the user to choose the categories for its game. Provide a default values.
+    def create_categories(self):
+        pass
+
+    def create_questions(self):
+        pass
+
+
+    # CREATE PLAYERS AND UPDATE THE NUMBER OF TOKENS
+    # Ask the user how many players he/she wants to create
     def get_number_of_players(self):
         try:
             num = int(input("How many players do you want to create (min. 2 - max. 6)?\n ").strip().lower())
@@ -19,12 +71,49 @@ class Game:
         except ValueError:
             print("Please enter a number.")
 
+    # Create players (as many as indicated with get_number_of_players)
     def create_players(self, number):
         for i in range(number):
-            player = Player()
+            player = Player(self)
             self.players.append(player)
             print(f"Player n. {i+1}, \"{player.name}\" created successfully.")
+            self.update_tokens(player)
         return self.players
-        
+    
+    # Update the number of tokens each time a player is created
+    def update_tokens(self, player):
+        # Add the token to the taken tokens
+        self.taken_tokens[player.name] = player.token
+        # Remove it from the available tokens
+        self.available_tokens.remove(player.token)
 
-game = Game()
+'''
+    # Prompt a question to the player
+    def ask_question(self, category):
+        # TODO: make num_correct an instance variable
+        num_correct = 0 # keeps score
+        # Choose a question
+        question = random.choice(list(self.questions[category]))
+        print(f"{question}")
+        # get the answer options of the questions picked
+        options = questions[category][question]
+        # the correct answer is always the first in the options list
+        correct_answer = options[0]
+        # shuffle the order of the options with a random sample that takes all the available options and print them
+        labeled_options = self.print_options(random.sample(options, k=len(options)))
+        # ERROR HANDLING: Reprompt the player if he/she gives an answer not in the options given
+        answer_checked = self.validate_option(
+            labeled_options,
+            f"\nChoice? ",
+            f"Please answer one of {', '.join(labeled_options)}"
+            )
+        answer = labeled_options[answer_checked]
+        if answer == correct_answer:
+            num_correct += 1
+            print('⭐ Correct!⭐')
+        else:
+            print(f"Sorry, incorrect answer. The answer is {correct_answer!r}, not {answer!r}")
+
+'''
+
+game1 = Game()
--- trivial.py
+++ trivial.py
@@ -1,115 +1,59 @@
 import random
-from string import ascii_lowercase
-from board import categories, squares
-from questions import questions
-
+from utility import validate_option, print_options
 
 class Player:
-    # Class var to keep track of the available tokens to be chosen
-    available_tokens = ['pink', 'yellow', 'orange', 'blue', 'brown', 'green']
+    # Initialize the player. Ask for name and token they want to choose.
+    def __init__(self, game):
+        self.game = game
+        self.name = self.choose_name()        
+        self.token = self.choose_token()
 
-    # Class var to keep track of the tokens already taken by other players
-    taken_tokens = {}
-
-    # Function to check if the input provided from the user is valid
-    # Takes the input prompt and value, the list where you want to check if the value exists, the error message to display if the input is not valid
-    def validate_input(self, validation_list, user_input, error_message):
-        while True:
-            label = input(user_input).strip().lower()
-            if label in validation_list:
-                return label
-            print(error_message)
     
-    # Function to print available options in format "a) option"
-    def print_options(self, options_list):
-        labeled_options = dict(zip(ascii_lowercase, options_list))
-        for label, option in labeled_options.items():
-            print(f" {label}) {option}")
-        return labeled_options
-
     # Player chooses name
     def choose_name(self):
         name = input('Choose a name for your player: ').strip()
+        print(f'Your player name is "{name}"')
         return name
 
     # Player chooses token
     def choose_token(self):
         # Print the available tokens
         print('These are the available tokens:')
-        labeled_available_tokens = self.print_options(Player.available_tokens)
+        labeled_available_tokens = print_options(self.game.available_tokens)
         # Validate the choice
-        token_checked = self.validate_input(
+        token_checked = validate_option(
             labeled_available_tokens,
             'Choose one token. Type the corresponding letter to make your choice: ',
             f"Please choose one of the available tokens: {', '.join(labeled_available_tokens)}"
         )
         # Store the valid chosen token value in a variable
         token = labeled_available_tokens[token_checked]
-        # Remove it from the available tokens
-        Player.available_tokens.remove(token)
-        # Add it to the taken_tokens
-        Player.taken_tokens[self.name] = token
-        print(f"Your token is {token}.")
-        return Player.taken_tokens[self.name]
+        print(f"Your token is \"{token}\".")
+        return token
 
+    # # simulate the player rolling the die
+    # def roll_die(self):
+    #     print('Rolling the die...')
+    #     # picks a random square from the board
+    #     square = random.choice(list(squares.keys()))
+    #     # check the corresponding category
+    #     category = squares[square]
+    #     print(f'Square {square}, category {squares[square]}')
+    #     # if square is roll again, call the function again
+    #     if category == categories['white']:
+    #         self.roll_die()
+    #     # if die goes on Start, the user must choose a category
+    #     elif category == categories['start']:
+    #         labeled_categories = self.print_options(list(questions))
+    #         # check if the provided category is valid
+    #         category_checked = self.validate_input(
+    #             labeled_categories,
+    #             f"\nChoice? ",
+    #             f"Please choose one of the categories {', '.join(labeled_categories)}"
+    #             )
+    #         # the category is chosen from the user
+    #         category = labeled_categories[category_checked]
 
-    # Initialize the player. Ask for name and token they want to choose.
-    def __init__(self):
-        self.name = self.choose_name()        
-        self.token = self.choose_token()
-
-
-    # Prompt a question to the player
-    def ask_question(self, category):
-        # TODO: make num_correct an instance variable
-        num_correct = 0 # keeps score
-        question = random.choice(list(questions[category]))
-        print(f"{question}")
-
-        # get the answer options of the questions picked
-        options = questions[category][question]
-        # the correct answer is always the first in the options list
-        correct_answer = options[0]
-        # shuffle the order of the options with a random sample that takes all the available options and print them
-        labeled_options = self.print_options(random.sample(options, k=len(options)))
-
-        # ERROR HANDLING: Reprompt the player if he/she gives an answer not in the options given
-        answer_checked = self.validate_input(
-            labeled_options,
-            f"\nChoice? ",
-            f"Please answer one of {', '.join(labeled_options)}"
-            )
-        answer = labeled_options[answer_checked]
-        if answer == correct_answer:
-            num_correct += 1
-            print('⭐ Correct!⭐')
-        else:
-            print(f"Sorry, incorrect answer. The answer is {correct_answer!r}, not {answer!r}")
-
-
-    # simulate the player rolling the die
-    def roll_die(self):
-        print('Rolling the die...')
-        # picks a random square from the board
-        square = random.choice(list(squares.keys()))
-        # check the corresponding category
-        category = squares[square]
-        print(f'Square {square}, category {squares[square]}')
-        # if square is roll again, call the function again
-        if category == categories['white']:
-            self.roll_die()
-        # if die goes on Start, the user must choose a category
-        elif category == categories['start']:
-            labeled_categories = self.print_options(list(questions))
-            # check if the provided category is valid
-            category_checked = self.validate_input(
-                labeled_categories,
-                f"\nChoice? ",
-                f"Please choose one of the categories {', '.join(labeled_categories)}"
-                )
-            # the category is chosen from the user
-            category = labeled_categories[category_checked]
-
-        # ask question from a category
-        self.ask_question(category)
+    #     # ask question from a category
+    #     self.ask_question(category)
         
